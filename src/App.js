f1e3f3ab491002c899bb4c7c3689eb92c15e6795
TheTwin96 <annielvallevalera96@outlook.com>
2023-03-06 17:41:41 -0600

    Project adapted to class component.

--- src/App.js
+++ src/App.js
@@ -1,39 +1,42 @@
 
 import './App.css';
-import { Testimony } from './components/Testimony.jsx';
+import Testimony from './components/Testimony.jsx';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <div className='main_container'>
-        <h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1>
-        <Testimony
-          image='Shawn'
-          name='Shawn Wang'
-          country='Singapur'
-          responsability='Ingeniero de Software'
-          company='Amazon'
-          testimony='Da miedo cambiar de carrera. Solo gané la confianza de que podía programar trabajando a través de los cientos de horas de lecciones gratuitas en freeCodeCamp. Dentro de un año tuve un trabajo de seis cifras como ingeniero de software. freeCodeCamp cambió mi vida.'
-        />
-        <Testimony
-          image='Sarah'
-          name='Sarah Chima'
-          country='Nigeria'
-          responsability='Ingeniera de Software'
-          company='ChatDesk'
-          testimony='freeCodeCamp fue la puerta de entrada a mi carrera como desarrollador de software. El plan de estudios bien estructurado llevó mis conocimientos de programación de un nivel de principiante total a un nivel muy seguro. Era todo lo que necesitaba para conseguir mi primer trabajo de desarrollador en una empresa increíble.'
-        />
-        <Testimony
-          image='Emma'
-          name='Emma Bostian'
-          country='Suecia'
-          responsability='Ingeniera de Software'
-          company='Spotify'
-          testimony='Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify.'
-        />        
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <div className='main_container'>
+          <h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1>
+          <Testimony
+            image='Shawn'
+            name='Shawn Wang'
+            country='Singapur'
+            responsability='Ingeniero de Software'
+            company='Amazon'
+            testimony='Da miedo cambiar de carrera. Solo gané la confianza de que podía programar trabajando a través de los cientos de horas de lecciones gratuitas en freeCodeCamp. Dentro de un año tuve un trabajo de seis cifras como ingeniero de software. freeCodeCamp cambió mi vida.'
+          />
+          <Testimony
+            image='Sarah'
+            name='Sarah Chima'
+            country='Nigeria'
+            responsability='Ingeniera de Software'
+            company='ChatDesk'
+            testimony='freeCodeCamp fue la puerta de entrada a mi carrera como desarrollador de software. El plan de estudios bien estructurado llevó mis conocimientos de programación de un nivel de principiante total a un nivel muy seguro. Era todo lo que necesitaba para conseguir mi primer trabajo de desarrollador en una empresa increíble.'
+          />
+          <Testimony
+            image='Emma'
+            name='Emma Bostian'
+            country='Suecia'
+            responsability='Ingeniera de Software'
+            company='Spotify'
+            testimony='Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify.'
+          />        
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/components/Testimony.jsx
+++ src/components/Testimony.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import '../style_sheets/Testimony.css';
 
-export function Testimony(props) {
-  return (
-    <div className="container_testimony">
-      <img className="image_testimony" src={require(`../images/Testimony_${props.image}.png`)} alt="" />
-      <div className='container_text_testimony'>
-        <p className='name_testimony'><b>{props.name}</b> en {props.country}</p>
-        <p className='responsibility_testimony'>{props.responsability} en <b>{props.company}</b></p>
-        <p className='text_testimony'>"{props.testimony}"</p>
+class Testimony extends React.Component {
+  render () {
+    return (
+      <div className="container_testimony">
+        <img 
+          className="image_testimony" 
+          src={require(`../images/Testimony_${this.props.image}.png`)} 
+          alt={`Foto de ${this.props.nombre}`} 
+        />
+        <div className='container_text_testimony'>
+          <p className='name_testimony'><b>{this.props.name}</b> en {this.props.country}</p>
+          <p className='responsibility_testimony'>{this.props.responsability} en <b>{this.props.company}</b></p>
+          <p className='text_testimony'>"{this.props.testimony}"</p>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
+
+export default Testimony;
